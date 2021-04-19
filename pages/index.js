1149354b94749e0c426946295c3e1ebf4262143b
Colby Fayock <colbyfayock@users.noreply.github.com>
2021-04-19 15:57:24 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Applitools Eyes GitHub Action & Netlify
+Test
         </h1>
 
         <div className={styles.grid}>
